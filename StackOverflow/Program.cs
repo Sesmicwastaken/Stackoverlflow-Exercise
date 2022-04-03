26bcee88d1b61f15b9c4b5a76d2f32819701f6c1
Sesmic <Sesmicwastaken@tutamail.com>
2022-04-03 20:57:00 +0530

    Initial_Commit

---
+++ StackOverflow/Program.cs
@@ -0,0 +1,78 @@
+﻿namespace StackOverflow
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            while (true)
+            {
+                Post post = new();
+
+                Console.Write("Enter the Post Title: ");
+                post.PostTitle = Console.ReadLine();
+
+                Console.Write("Enter the Post Discription: ");
+                post.PostDiscription = Console.ReadLine();
+
+                Console.Write("Enter the post creation date: ");
+                post.PostCreationTime = Convert.ToDateTime(Console.ReadLine());
+
+
+                Stackoverflow.StorePosts = post;
+
+                Console.Write("\nNo. of posts: " + Stackoverflow.TotalPosts);
+
+
+
+
+                Console.WriteLine("\n\nWhich post do you wish to see?");
+                int postID = Convert.ToInt32(Console.ReadLine());
+
+
+                Stackoverflow.ShowPost(postID);
+                while (true)
+                {
+                    Console.WriteLine("\nUpvote and Downvote? (Enter \"No\" if you don't want to)");
+                    string input = Console.ReadLine();
+
+                    if (String.IsNullOrWhiteSpace(input))
+                    {
+                        throw new ArgumentNullException(input);
+                    }
+
+                    if (input.Trim().ToLower() == "upvote" || input.Trim().ToLower() == "downvote")
+                    {
+                        post.UpvoteDownvote(input.Trim().ToLower());
+                        Console.Clear();
+                        Stackoverflow.ShowPost(postID);
+                    }
+                    else if (input.Trim().ToLower() == "no")
+                    {
+                        break;
+                    }
+
+
+                }
+
+                Console.WriteLine("\nDo you wish to add more posts? (\"Yes\" or \"No\")");
+                string input2 = Console.ReadLine();
+                if (String.IsNullOrWhiteSpace(input2))
+                {
+                    throw new ArgumentNullException(input2);
+                }
+                if (input2.Trim().ToLower() == "yes")
+                {
+                    Console.Clear();
+                    continue;
+                }
+                else if (input2.Trim().ToLower() == "no")
+                {
+                    return;
+                }
+
+            }
+
+
+        }
+    }
+}
---
+++ StackOverflow/Stackoverflow.cs
@@ -0,0 +1,40 @@
+﻿using System.Text;
+
+namespace StackOverflow
+{
+    public class Stackoverflow
+    {
+
+        private static readonly List<Post> _posts = new();
+
+        public static Post StorePosts
+        {
+            set
+            {
+                _posts.Add(value);
+            }
+        }
+        public static int TotalPosts
+        {
+            get
+            {
+                return _posts.Count;
+            }
+
+        }
+
+        public static void ShowPost(int postID)
+        {
+            if (postID - 1 >= _posts.Count)
+            {
+                throw new NullReferenceException("\nPost Id is Invalid.");
+            }
+            StringBuilder PostFormat = new();
+            PostFormat.Append("\nTitle: " + _posts[postID - 1].PostTitle).AppendLine().AppendLine();
+            PostFormat.Append("Asked: " + _posts[postID - 1].PostCreationTime).AppendLine().AppendLine();
+            PostFormat.Append("Description: " + _posts[postID - 1].PostDiscription).AppendLine().AppendLine();
+            PostFormat.Append("Votes: " + _posts[postID - 1].NoOfVotes);
+            Console.WriteLine(PostFormat);
+        }
+    }
+}
