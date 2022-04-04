5390d68b14c69af05e2c059673e6d1bf28eeac6f
Alexis C. GRIDEL <alexis.gridel@pm.me>
2022-04-04 17:55:53 +0200

    refactor: namespace to file scope

--- StackOverflow/Post.cs
+++ StackOverflow/Post.cs
@@ -1,83 +1,82 @@
-﻿namespace StackOverflow
+﻿namespace StackOverflow;
+
+public class Post
 {
-    public class Post
+    private string _postTitle;
+    private string _postDiscription;
+    private DateTime _postCreationTime;
+    private int _votes = 0;
+
+
+    public string PostTitle
     {
-        private string _postTitle;
-        private string _postDiscription;
-        private DateTime _postCreationTime;
-        private int _votes = 0;
-
-
-        public string PostTitle
+        get { return _postTitle; }
+        set
         {
-            get { return _postTitle; }
-            set
+            if (!String.IsNullOrWhiteSpace(value))
+            { _postTitle = value; }
+            else
             {
-                if (!String.IsNullOrWhiteSpace(value))
-                { _postTitle = value; }
-                else
-                {
-                    throw new ArgumentNullException(value);
-                }
+                throw new ArgumentNullException(value);
             }
         }
-
-        public string PostDiscription
-        {
-            get { return _postDiscription; }
-            set
-            {
-                if (!String.IsNullOrWhiteSpace(value))
-                { _postDiscription = value; }
-                else
-                {
-                    throw new ArgumentNullException(value);
-                }
-            }
-        }
-        public DateTime PostCreationTime
-        {
-            get { return _postCreationTime; }
-            set
-            {
-
-                _postCreationTime = value;
-            }
-        }
-        public int NoOfVotes
-        {
-            get
-            {
-                return _votes;
-            }
-        }
-        public void UpvoteDownvote(string input)
-        {
-
-
-            if (String.IsNullOrWhiteSpace(input) || input != "upvote" && input != "downvote")
-            {
-                throw new ArgumentException("Invalid Input");
-            }
-
-            if (input.Trim().ToLower() == "upvote")
-            {
-                UpVote();
-            }
-            else if (input.Trim().ToLower() == "downvote")
-            {
-                DownVote();
-            }
-        }
-        internal void UpVote()
-        {
-            _votes += 1;
-        }
-        internal void DownVote()
-        {
-            _votes -= 1;
-        }
-
-
     }
+
+    public string PostDiscription
+    {
+        get { return _postDiscription; }
+        set
+        {
+            if (!String.IsNullOrWhiteSpace(value))
+            { _postDiscription = value; }
+            else
+            {
+                throw new ArgumentNullException(value);
+            }
+        }
+    }
+    public DateTime PostCreationTime
+    {
+        get { return _postCreationTime; }
+        set
+        {
+
+            _postCreationTime = value;
+        }
+    }
+    public int NoOfVotes
+    {
+        get
+        {
+            return _votes;
+        }
+    }
+    public void UpvoteDownvote(string input)
+    {
+
+
+        if (String.IsNullOrWhiteSpace(input) || input != "upvote" && input != "downvote")
+        {
+            throw new ArgumentException("Invalid Input");
+        }
+
+        if (input.Trim().ToLower() == "upvote")
+        {
+            UpVote();
+        }
+        else if (input.Trim().ToLower() == "downvote")
+        {
+            DownVote();
+        }
+    }
+    internal void UpVote()
+    {
+        _votes += 1;
+    }
+    internal void DownVote()
+    {
+        _votes -= 1;
+    }
+
+
 }
--- StackOverflow/Stackoverflow.cs
+++ StackOverflow/Stackoverflow.cs
@@ -1,40 +1,39 @@
 ﻿using System.Text;
 
-namespace StackOverflow
+namespace StackOverflow;
+
+public class Stackoverflow
 {
-    public class Stackoverflow
+
+    private static readonly List<Post> _posts = new();
+
+    public static Post StorePosts
     {
-
-        private static readonly List<Post> _posts = new();
-
-        public static Post StorePosts
+        set
         {
-            set
-            {
-                _posts.Add(value);
-            }
+            _posts.Add(value);
         }
-        public static int TotalPosts
+    }
+    public static int TotalPosts
+    {
+        get
         {
-            get
-            {
-                return _posts.Count;
-            }
-
+            return _posts.Count;
         }
 
-        public static void ShowPost(int postID)
+    }
+
+    public static void ShowPost(int postID)
+    {
+        if (postID - 1 >= _posts.Count)
         {
-            if (postID - 1 >= _posts.Count)
-            {
-                throw new NullReferenceException("\nPost Id is Invalid.");
-            }
-            StringBuilder PostFormat = new();
-            PostFormat.Append("\nTitle: " + _posts[postID - 1].PostTitle).AppendLine().AppendLine();
-            PostFormat.Append("Asked: " + _posts[postID - 1].PostCreationTime).AppendLine().AppendLine();
-            PostFormat.Append("Description: " + _posts[postID - 1].PostDiscription).AppendLine().AppendLine();
-            PostFormat.Append("Votes: " + _posts[postID - 1].NoOfVotes);
-            Console.WriteLine(PostFormat);
+            throw new NullReferenceException("\nPost Id is Invalid.");
         }
+        StringBuilder PostFormat = new();
+        PostFormat.Append("\nTitle: " + _posts[postID - 1].PostTitle).AppendLine().AppendLine();
+        PostFormat.Append("Asked: " + _posts[postID - 1].PostCreationTime).AppendLine().AppendLine();
+        PostFormat.Append("Description: " + _posts[postID - 1].PostDiscription).AppendLine().AppendLine();
+        PostFormat.Append("Votes: " + _posts[postID - 1].NoOfVotes);
+        Console.WriteLine(PostFormat);
     }
 }
